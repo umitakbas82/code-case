9cd415af7e3d2c430335e04ff6058db701309069
UmitAkbas <umit.akbas@hotmail.com.tr>
2025-10-17 00:17:44 +0300

    login component added and worked

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+
+  login() {
+    if (!this.username || !this.password) {
+      this.errorMessage = 'Kullanııcı adı ve şifre boş olamaz';
+      return;
+    }
+
+    this.authService.login(this.username, this.password).subscribe(user => {
+      if (user) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.errorMessage = 'Geçersiz kullanıcı adı veya şifre'
+      }
+    })
+  }
 }
