4c1c683d833714ade3e95f74fb25328904b7b076
UmitAkbas <umit.akbas@hotmail.com.tr>
2025-10-17 00:07:25 +0300

    dashborad, annotation , toolbar compnents syle added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RouterLink } from '@angular/router';
+import { User } from '../../models/userModel';
+import { Task } from '../../models/taskModel';
+import { AuthService } from '../../services/auth.service';
+import { TaskService } from '../../services/task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  public currentUser$: Observable<User | null>;
+  public tasks$!: Observable<Task[]>;
+
+  constructor(public authService: AuthService, private taskService: TaskService) {
+    this.currentUser$ = this.authService.currentUser$
+  }
+
+  ngOnInit(): void {
+    //anlık kullanıcıyı al
+    const currentUser = this.authService.currentUserValue;
+    //kullanıcı varsa görevlerini iste
+    if (currentUser) {
+      this.tasks$ = this.taskService.getTaskForUser(currentUser.id)
+    }
+
+  }
+
+
+  logout() {
+    this.authService.logout();
+  }
+
+
+
+
+
 
 }
