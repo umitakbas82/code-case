8220a19e7552a278443a21945dfbafae87552df9
UmitAkbas <umit.akbas@hotmail.com.tr>
2025-10-16 23:51:07 +0300

    refactor: update app component to use router outlet and implement auth guard for route protection

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    if (authService.isAuthenticated()) {
+        return true;
+    }
+    router.navigate(['/login']);
+    return false;
 };
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+import { Task } from '../models/taskModel';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
+  getTaskForUser(userId: number): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}/tasks?userId=${userId}`);
+  }
+
 }
